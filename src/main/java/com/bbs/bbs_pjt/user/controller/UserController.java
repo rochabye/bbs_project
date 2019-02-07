7ca9c295b50ea8befd3bdab1629f04c296c01218
rochabye <reng731@gmail.com>
2019-02-07 16:55:55 +0900

    java : add login fail alert, logout action

--- src/main/java/com/bbs/bbs_pjt/user/controller/UserController.java
+++ src/main/java/com/bbs/bbs_pjt/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.bbs.bbs_pjt.user.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,12 +20,11 @@ public class UserController {
 	UserService service;
 	
 	@RequestMapping( "/join" )
-	public String userJoin( Model model, HttpServletRequest request ) {
+	public String userJoin() {
 		return "join";
 	}
 	@RequestMapping( value="/joinAction", method = RequestMethod.POST )
 	public String userJoinAction( User user ) {
-		System.out.print( "join action" ) ;
 		service.userRegister( user.getUserID(), user.getUserPassword(), user.getUserName(), 
 				user.getUserGender(), user.getUserEmail() );
 		
@@ -32,30 +32,45 @@ public class UserController {
 	}
 	
 	@RequestMapping( "/login" )
-	public String login( Model model, HttpServletRequest request ) {
-		System.out.print( "login!!" ) ;
+	public String login() {
 		return "login";
 	}
 	
 	@RequestMapping( value="/loginAction", method = RequestMethod.POST )
-	public String userLoginAction( User user ) {
-		System.out.print( "login action " + user.getUserID() + user.getUserPassword()  ) ;
+	public String userLoginAction( Model model, User user, HttpSession session ) {
+		
 		int result = service.userSearch( user.getUserID(), user.getUserPassword() );
-		System.out.print( "!!result " + result ) ;
 		if ( result == 1 ){
+			session.setAttribute( "userID", user.getUserID() );
 			return "loginOk";
- 			// login success. set session. go main 
- 			//session.setAttribute("userID", user.getUserID());
  		}
  		else if ( result == 0 ) {
+ 			model.addAttribute("msg", "비밀번호가 틀렸습니다."); 
+ 			model.addAttribute("url", "login"); 
+ 			return "redirect";
  			// wrong password 
  		}
  		else if ( result == -1 ) {
+ 			model.addAttribute("msg", "아이디가 존재하지 않습니다."); 
+ 			model.addAttribute("url", "login"); 
+ 			return "redirect";
  			// no id 
  		}
  		else if ( result == -2 ) {
+ 			model.addAttribute("msg", "데이터베이스 오류가 발생했습니다."); 
+ 			model.addAttribute("url", "login"); 
+ 			return "redirect";
  			// database error 
  		}
-		return "loginOK";
+		model.addAttribute("msg", "알 수 없는 오류가 발생했습니다."); 
+		model.addAttribute("url", "login"); 
+		return "redirect";
 	}
+	
+	@RequestMapping( "/logout" )
+	public String logout( HttpSession session ) {
+		session.invalidate();
+		return "main";
+	}
+	
 }
