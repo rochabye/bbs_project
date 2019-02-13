2ad03fe54589b7a5a1d67abf47270b479a2f5943
rochabye <reng731@gmail.com>
2019-02-13 18:01:18 +0900

    java : add package for paging

---
+++ src/main/java/com/bbs/bbs_pjt/commons/paging/PageMaker.java
@@ -0,0 +1,73 @@
+package com.bbs.bbs_pjt.commons.paging;
+
+public class PageMaker {
+	
+	private int totalCount;
+	private int startPage;
+	private int endPage;
+	private boolean prev;
+	private boolean next;
+	
+	private int displayPageNum = 10;
+	
+	private Criteria criteria;
+	
+	
+	private void calcData() { 
+		endPage = ( int ) ( Math.ceil( criteria.getPage() / (double) displayPageNum ) * displayPageNum );
+		startPage = ( endPage - displayPageNum ) + 1;
+		
+		int tempEndPage = ( int ) ( Math.ceil( totalCount / ( double) criteria.getPerPageNum() ) );
+		
+		if ( endPage > tempEndPage ) {
+			endPage = tempEndPage;
+		}
+		
+		prev = startPage == 1 ? false : true;
+		next = endPage * criteria.getPerPageNum() >= totalCount ? false : true;
+	
+	}
+	public void setCriteria( Criteria criteria ) {
+		this.criteria = criteria;
+	}
+	public Criteria getCriteria() {
+		return criteria;
+	}
+	
+	public int getTotalCount() {
+		return totalCount;
+	}
+	public void setTotalCount( int totalCount ) {
+		this.totalCount = totalCount;
+		calcData();
+	}
+	
+	public int getEndPage() {
+		return endPage;
+	}
+	public void setEndPage( int endPage ) {
+		this.endPage = endPage;
+	}
+	
+	public boolean isPrev() {
+		return prev;
+	}
+	public void setPrev( boolean prev ) {
+		this.prev = prev;
+	}
+	
+	public boolean isNex() { 
+		return next;
+	}
+	public void setNext( boolean next ) {
+		this.next = next;
+	}
+	
+	public int getDisplayPageNum() {
+		return displayPageNum;
+	}
+	public void setDisplayPageNum( int displayPageNume ) {
+		this.displayPageNum = displayPageNum;
+	}
+	
+}
